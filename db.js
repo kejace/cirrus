f9973d49049c92057a233cc2ec683e6db2982800
kejace <kejace@gmail.com>
2016-05-06 01:03:19 -0400

    Associations

--- db.js
+++ db.js
@@ -25,10 +25,12 @@ var address_state_ref = AddressStateRef(Conn, Sequelize);
 //raw_transaction.belongsTo(block_data_ref);
 //block_data_ref.hasMany(raw_transaction);
 //address_state_ref.hasMany(raw_transaction);
-address_state_ref.belongsTo(raw_transaction, {foreignKey: 'to_address'});
+//address_state_ref.belongsTo(raw_transaction, {foreignKey: 'to_address'});
 //address_state_ref.hasOne(address_state_ref, {foreignKey: 'to_address'});
 //raw_transaction.hasOne(address_state_ref, {foreignKey: 'to_address'});
 //raw_transaction.hasOne(address_state_ref, {foreignKey: 'from_address'});
+
+raw_transaction.belongsTo(address_state_ref, {foreignKey: 'to_address'});
 
 // Conn.sync({ force: true }).then(()=> {
 //   _.times(10, ()=> {
